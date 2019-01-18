a0fef8fee7d5a65debb1085526ec3ea70c372e78
Guilherme Santos <sntguilherme@gmail.com>
2019-01-18 14:22:51 -0200

    Lazy Loading Components

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,22 @@ import React from 'react';
 
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
-import loadDB from '../lib/load-db'
 
 const PostLink = (props) => (
     <li>
-        <Link as={`/p/${props.id}`} href={`/post?id=${props.id}`}>
+        <Link href={`/p/${props.id}`}>
             <a>{props.title}</a>
         </Link>
     </li>
 )
 
-const Index = ({ stories }) => (
+export default () => (
     <Layout>
-        <h1>Hacker News - Latest</h1>
+        <h1>My Blog</h1>
         <ul>
-            {stories.map(story => (
-                <PostLink
-                    key={story.id}
-                    id={story.id}
-                    title={story.title}
-                />
-            ))}
+            <PostLink id="hello-nextjs" title="Hello Next.js"/>
+            <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
         </ul>
     </Layout>
 )
-
-Index.getInitialProps = async () => {
-    const db = await loadDB()
-
-    const ids = await db.child('topstories').once('value')
-    let stories = await Promise.all(
-        ids.val().slice(0, 10).map(id => db
-            .child('item')
-            .child(id)
-            .once('value')
-        )
-    )
-
-    stories = stories.map(s => s.val())
-
-    return { stories }
-}
-
-export default Index
