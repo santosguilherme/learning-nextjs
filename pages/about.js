0cdc14e22ab6d2737ed826f32d79d6dd10eabcb4
Guilherme Santos <sntguilherme@gmail.com>
2024-04-12 15:14:19 -0300

    chore: resetting branch

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Layout from '../components/MyLayout.js'
-
-export default () => (
-    <Layout>
-        <p>This is the about page</p>
-    </Layout>
-)
--- pages/p/learn-nextjs.js
+++
@@ -1,8 +0,0 @@
-import WithPost from '../../lib/with-post'
-
-export default WithPost({
-    title: 'Learn Next.js website is awesome',
-    content: `
-To start learning Next.js, simply visit: <https://learnnextjs.com>
-  `
-})
